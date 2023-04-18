3c5cdeedc669d02e723066305617ee165648362e
Simon Giraldo <simongiraldoc@gmail.com>
2023-04-18 17:17:04 -0500

    Cambios antes de susy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates/', static_folder='../static/')
 
 @app.route('/')
 def main():
-    return 'Hello world, EIAPI'
+    response = make_response(redirect('/login'))
+    return response
+
+@app.route('/login')
+def login():
+    return render_template('documentation.html')
