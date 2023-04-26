577d7a04f4184da0b1e32023fdf9dc19cc91582e
Simon Giraldo <simongiraldoc@gmail.com>
2023-04-26 01:38:47 -0500

    Bug rest response fixed

--- api/app.py
+++ api/app.py
@@ -18,23 +18,23 @@ def main():
 def estudiantes():
     request_data = json.loads(request.data)
     user_name = request_data['username']
-    name = request_data['nombre']
+    nombre = request_data['nombre']
     edad = int(request_data['edad'])
     pregrado = request_data['pregrado']
     semestre_actual  = int(request_data['semestre_actual'])
     transporte = request_data['transporte']
-    if validateStudent(user_name, name, edad, pregrado, semestre_actual, transporte):
+    if validateStudent(user_name, nombre, edad, pregrado, semestre_actual, transporte):
         with conection.connect() as con:
-            new_student = Student(username=user_name, nombre=name, edad=edad, pregrado=pregrado, semestre_actual=semestre_actual)
+            new_student = Student(username=user_name, nombre=nombre, edad=edad, pregrado=pregrado, semestre_actual=semestre_actual)
             ses.add(new_student)
             try:
                 ses.commit()
             except:
-                return jsonify({"error: true"}, {"Message": "error, wrong fields"})
+                return jsonify({"Message": "error, wrong fields"})
             
         succes = f'User {user_name} created'
-        return jsonify({"error: false"}, {f"Message": succes})
-    return jsonify({"error: true"}, {"Message": "error, wrong fields"})
+        return jsonify({f"Message": succes})
+    return jsonify({"Message": "error, wrong fields"})
 
 def validateStudent(user_name, name, edad, pregrado, semestre_actual, transporte):
      return len(name) != 0 and len(name) < 200 and len(user_name) != 0 and len(user_name) < 200 and len(pregrado) != 0 and len(pregrado) < 100 and isinstance(edad, int) and isinstance(semestre_actual, int) and len(transporte) != 0 and len(transporte) < 100
